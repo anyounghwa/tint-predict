db1064fcb8ac620274dd8aa2f4aeec39a2332107
younghwa <e89cokr@naver.com>
2025-05-30 17:08:19 +0900

    keras->h5로 수정함

---
+++ flask/02_정적동적라우팅/app_static.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)  #앱 인스턴스 생성
+
+@app.route("/")  
+def home():
+    return "<h1>Hello world : home</h1>"
+
+@app.route("/hello")   #http://127.0.0.1:5000/hello
+def hello():
+    return "<h1>Hello world : hello</h1>"
+
+if __name__ == "__main__":  #main코드라면
+    app.run()
